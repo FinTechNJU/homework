8694729caa085eb221785c22ae7e85260f61d72a
Terry Stanley <37290277+FinTechNJU@users.noreply.github.com>
2019-05-31 15:37:38 +0800

    Update RandomForestMain.py

--- RandomForestMain.py
+++ RandomForestMain.py
@@ -1,104 +1,125 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon May 20 15:37:25 2019
-
-@author: lilangyi
-"""
-
-import numpy as np 
-import pandas as pd
+from __future__ import division
 from pandas import DataFrame
-
 from sklearn import tree
-from sklearn import preprocessing
-from sklearn.model_selection import KFold
-#import math
 from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import KFold
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import random
 
-
-def loadData(path):
-    
+def loadData(path):  
     fr=open("/Users/lilangyi/Downloads/"+path)
     line= fr.readline()
     if line[0]=="|":
         pass
     else:
-        fr=open("/Users/lilangyi/Downloads/"+path)
-        
+        fr=open("/Users/lilangyi/Downloads/"+path)     
     rd=[]
-    #fr=open("/Users/lilangyi/Downloads/"+path)
     for line in fr.readlines():
-        #print(line)
         arr=line.strip().split(", ")
-        #print(arr)
         rd.append(arr)
-    #yl,xl=len(rd),len(rd[0])
     xMat=DataFrame(rd)  
-    #xMat=xMat.applymap(str) 
-    lst=[0,2,4,10,11,12]
-    for i in lst:
-        xMat[i]=xMat[i].astype(np.int64)    
     for i in range(14):
-        if i not in lst:
-            #xMat[i]=xMat[i].apply(str)
-            #xMat[i]=xMat[i].astype(str)
-            xMat[i]=xMat[i].apply(hash)
-    #print(xMat)  
+        xMat[i]=xMat[i].apply(hash)
+        xMatIMax=max(xMat[i])
+        xMat[i]=xMat[i].apply(lambda x:x/xMatIMax)
+        xMat[i]=xMat[i].apply(lambda x:float("%.2f"%x))
     xMat[14]=xMat[14].replace("<=50K",-1)
     xMat[14]=xMat[14].replace(">50K",1)
     xMat[14]=xMat[14].replace("<=50K.",-1)
-    xMat[14]=xMat[14].replace(">50K.",1)
-    #print(xMat)    
-    return xMat.iloc[:,:-1],xMat.iloc[:,-1]
+    xMat[14]=xMat[14].replace(">50K.",1)  
+    return np.mat(xMat.iloc[:-1,:-1]),np.array(xMat.iloc[:-1,-1])
+
+class RandomForest:
+
+    def __init__(self,trCounts=10,trDimension=0.5):
+        self.treeCounts=trCounts
+        self.treeDimension=trDimension
+        self.treeList=[]
+        
+    # 随机抽取样本，样本数量与原训练样本集一样，维度为 int(pow(m - 1,self.treeDimension))
+    def baggingDataSet(self,dataSet):
+        #样本自助采样，随机选择特征
+        n, m = dataSet.shape
+        features = random.sample(dataSet.columns.values[:-1].tolist(), int(pow(m - 1,self.treeDimension)))
+        features.append(dataSet.columns.values[-1])
+        rows = [random.randint(0, n-1) for _ in range(n)]
+        trainData = dataSet.iloc[rows][features[:-1]]
+        trainLabel = dataSet.iloc[rows][features[-1]]
+        return trainData, trainLabel
+        
+    def rfTrain(self,dataMat,dataLabel): 
+        for i in range(self.treeCounts):      
+            decisionTree = tree.DecisionTreeClassifier(max_depth=5) 
+            decisionTree.fit(np.mat(dataMat), np.array(dataLabel))
+            self.treeList.append([decisionTree,\
+                             dataMat.columns.values,
+                             dataLabel.index.values])
+        return
     
-dataMat,labelMat=loadData("test.data")
-testMat,testlabel=loadData("test.test") 
-print("Data Loaded!")
+    def rfPredict(self,predMat):   
+        # 对测试样本分类
+        labelPred = []
+        for tr in self.treeList:
+            testLabel = tr[0].predict(predMat.T[tr[1]].T)
+            labelPred.append(testLabel.astype(int))         
+        predLabel=[]
+        for i in range(len(labelPred[0])):
+            tmpList=[]
+            for item in labelPred:
+                tmpList.append(item[i])
+            # 投票选择最终类别
+            labelDict = {}
+            for label in tmpList:
+                labelDict[label] = labelDict.get(label, 0) + 1            
+            sortClass = sorted(labelDict.items(), key=lambda item: item[1])
+            predLabel.append(sortClass[-1][0])     
+        return np.array(predLabel)
+    
+    def rfEvaluation(self,testMat,testLabel):
+        rfClassEst,expon = 0, 0.4
+        predLabel = self.rfPredict(testMat)
+        rfClassEst = roc_auc_score(testLabel,predLabel)
+        return rfClassEst**expon
+    
 
-
-#
-def randomforest(dataMat,labelMat,testdata,forecast_data,num_trainer):
-    trainer_result=[]
-    forecast_result=[]
-    for i in range(num_trainer):
-        clf = tree.DecisionTreeClassifier(criterion="entropy",max_depth=1,max_features="auto")
-        clf.fit(dataMat,labelMat)
-        
-        trainer_pro=clf.predict_proba(testdata)
-        trainer_pro_z=[]
-        for item in trainer_pro:
-            trainer_pro_z.append(item[1])
-        trainer_result.append(trainer_pro_z)
-        
-        forcast_pro=clf.predict_proba(forecast_data)
-        forcast_pro_z=[]
-        for item in forcast_pro:
-            forcast_pro_z.append(item[1])
-        forecast_result.append(forcast_pro_z)
-        
-    return trainer_result,forecast_result
-
-#5折交叉
-auc_z=[]
-for trainer_num in range(1,10):
-    auc=0
-    kfold= KFold(n_splits=5,random_state =None)
+if __name__=="__main__":
+    print(">>>Process 1:   The processing of data\n>>>Description: To load the given data")
+#    fakeData,realData=True,False
+    realData,fakeData=True,False
+    
+    if(fakeData):
+        dataMat,labelMat=loadData("test.data")
+        testMat,testlabel=loadData("test.test") 
+    elif(realData):
+        dataMat,labelMat=loadData("adult.data")
+        testMat,testlabel=loadData("adult.test")  
+    df = pd.concat([pd.DataFrame(dataMat),pd.DataFrame(labelMat)],axis=1)
+    df=np.mat(df)
+    df=pd.DataFrame(df)
+    print(">>>Result:      Data loaded!\n")
+    
+    print(">>>Process 2:   The KFold of data\n>>>Description: To process the loaded data")
+    numTrees, stepGap = 50, 1
+    treeDimension, i= 0.5, 0
+    forestList=[]
+    kfold= KFold(n_splits=5,random_state =None)    
+    numTrees+=i*stepGap
+    treeDimension+=0.05*stepGap
+    rf = RandomForest(numTrees,treeDimension) #生成对象
+    baggingData, bagginglabels = rf.baggingDataSet(df)
     for train,test in kfold.split(dataMat):
-        
-        train_result,forecast_result=\
-        randomforest(dataMat[train],\
-        labelMat[train],\
-        dataMat[test],\
-        testMat,\
-        trainer_num)
-        
-        for i in range(trainer_num):
-            if(i==0):
-                trainer_result_z=np.array(trainer_result[i])/trainer_num
-            else:
-                trainer_result_z=trainer_result_z+np.array(trainer_result[i])/trainer_num
-           
-        auc+=roc_auc_score(np.array(labelMat)[test],trainer_result_z)
-    auc_z.append(auc/5)
+        numTrees+=i*stepGap
+        rf.treeDimension+=0.05*stepGap
+        rf.rfTrain(baggingData, bagginglabels)
+        aucEst = rf.rfEvaluation(testMat,testlabel)
+        forestList.append({"rf":rf,"numTrees":numTrees,\
+                           "aucEst":aucEst})
+        i+=1
+    aucList = []
+    for item in forestList:
+        aucList.append(item["aucEst"])
+    aucIndex=aucList.index(max(aucList))    
+    print(">>>Detail:      The AUC of the test set is : %f "%aucList[aucIndex])
